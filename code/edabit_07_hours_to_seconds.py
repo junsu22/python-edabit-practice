1eb9af74ae9f5c476016c62bff2c2b98beff7cc0
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2026-01-11 00:43:48 +0900

    Edabit hours to seconds practice

---
+++ code/edabit_07_hours_to_seconds.py
@@ -0,0 +1,13 @@
+# 시간을 초로 변환하기
+# 입력 : 시간 , 출력 : 초
+
+# 1시간 : 60초 
+# 1분 : 60초 
+
+# 1hour  = 60 * 60 = 3600sec
+
+def how_many_seconds(hours):
+    return hours * 3600
+
+
+print(how_many_seconds(5)) # 18000
